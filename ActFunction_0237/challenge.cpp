48219d054c49d38f0e763c03e292b9976c6e862f
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-02 20:54:12 +0700

    ActFunction_0237

--- ActFunction_0237/challenge.cpp
+++ ActFunction_0237/challenge.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 using namespace std;
 float Luas, d1, d2;
+
+void ProsedurInputData(){
+    cout << "Masukkan diagonal 1 : ";
+    cin >> d1;
+    cout << "Masukkan diagonal 2 : ";
+    cin >> d2;
+}
