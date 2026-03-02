35a56ef23e78d095e4dd57b04d8bee3bf73194bf
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-02 20:44:24 +0700

    ActFunction_0237

--- ActFunction_0237/pertemuan3.cpp
+++ ActFunction_0237/pertemuan3.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 //deklarasi variabel global
 int panjang, lebar;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+}
